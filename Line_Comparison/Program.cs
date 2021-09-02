9c6fda2e43546881f61a630eb20ae212bd027e85
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-02 15:41:48 +0530

    Length calculated UC-1

---
+++ Line_Comparison/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Line_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparison Computation Program");
+            int x1, x2, y1, y2;
+            double line_One;
+            x1 = 2; y1 = 3; x2 = 3;y2 = 2;
+
+            int Equation = ((x2 - x1) ^ 2 * (y2 - y1) ^ 2);
+            line_One = Math.Abs((0.5) * Equation);
+
+            Console.WriteLine("Length of line :" + line_One);
+
+        }
+    }
+}
